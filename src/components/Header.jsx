ab56e4eb3a114b48ea7559020a2999a90b868061
SanjanaMSunil <sanjanamsunil52@gmail.com>
2024-02-26 02:10:36 +0530

    'showhide'

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import * as mdb from 'mdb-ui-kit';
+import React from 'react'
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand
+  } from 'mdb-react-ui-kit';
+ 
+function Header() {
+  return (
+    <div>
+        <MDBNavbar light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>
+            <img
+              src='https://cdn.dribbble.com/users/141880/screenshots/2112209/eyeball-menu.gif'
+              height='30'
+              alt=''
+              loading='lazy' 
+            />
+            SHOW-TASK
+          </MDBNavbarBrand>
+        </MDBContainer>
+      </MDBNavbar>
+      
+    </div>
+    
+  )
+}
+
+export default Header
